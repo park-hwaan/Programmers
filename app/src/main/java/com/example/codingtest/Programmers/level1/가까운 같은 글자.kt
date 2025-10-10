12a6c64eaea9749d8f7c18fb70cc3e28af968acb
park-hwaan <parkhwan1129@gmail.com>
2025-10-10 23:33:42 +0900

    가까운 같은 글자

--- app/src/main/java/com/example/codingtest/Programmers/level1/가까운 같은 글자.kt
+++ app/src/main/java/com/example/codingtest/Programmers/level1/가까운 같은 글자.kt
@@ -1,5 +1,20 @@
 package com.example.codingtest.Programmers.level1
 
-fun main(){
+fun main() {
+    val s = "foobar"
+    val answer = IntArray(s.length) { -1 }
+    val charMap = mutableMapOf<Char, Int>()
+
+
+    for (index in s.indices) {
+        val c = s[index]
+        if (charMap.containsKey(c)) {
+            answer[index] = index - charMap[c]!!
+            charMap[c] = index
+        } else {
+            answer[index] = -1
+            charMap[c] = index
+        }
+    }
 
 }
